c45266efe7ffcf42b3dd56d9206d8b74dd9f7f02
Nassem <nassemmubarak@gmail.com>
2023-03-13 11:33:37 +0200

    chang app

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,68 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomePage from "../views/HomePage.vue";
+import PoliticsPage from "../views/PoliticsPage.vue";
+import EducationApp from "../views/EducationPage.vue";
+import SportPage from "../views/SportPage.vue";
+import TravelPage from "../views/TravelPage.vue";
+import VlogsPage from "../views/VlogsPage.vue";
+import HealthPage from "../views/HealthPage.vue";
+import DetailsNews from "../views/DetailsNews.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "HomePage",
+    component: HomePage,
   },
   {
-    path: '/about',
-    name: 'about',
+    path: "/travel",
+    name: "TravelPage",
+    component: TravelPage,
+  },
+  {
+    path: "/health",
+    name: "HealthPage",
+    component: HealthPage,
+  },
+  {
+    path: "/details",
+    name: "DetailsNews",
+    component: DetailsNews,
+  },
+  {
+    path: "/vlogs",
+    name: "VlogsPage",
+    component: VlogsPage,
+  },
+  {
+    path: "/sport",
+    name: "SportPage",
+    component: SportPage,
+  },
+  {
+    path: "/politics",
+    name: "PoliticsPage",
+    component: PoliticsPage,
+  },
+  {
+    path: "/education",
+    name: "EducationApp",
+    component: EducationApp,
+  },
+  {
+    path: "/about",
+    name: "about",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/views/HealthPage.vue
@@ -0,0 +1,228 @@
+<template>
+    <div class="health-page">
+        <!-- end header -->
+
+        <div class="page-title wb">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+                        <h2><i class="fa fa-user-md bg-grey"></i> Health <small class="hidden-xs-down hidden-sm-down">Nulla felis eros, varius sit amet volutpat non. </small></h2>
+                    </div><!-- end col -->
+                    <div class="col-lg-4 col-md-4 col-sm-12 hidden-xs-down hidden-sm-down">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item"><a href="#">Blog</a></li>
+                            <li class="breadcrumb-item active">Fashion</li>
+                        </ol>
+                    </div><!-- end col -->                    
+                </div><!-- end row -->
+            </div><!-- end container -->
+        </div><!-- end page-title -->
+
+        <section class="section wb">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <div class="page-wrapper">
+                            <div class="portfolio row">
+                                <div class="pitem item-w1 item-h1">
+                                    <div class="blog-box">
+                                        <div class="post-media">
+                                            <a href="single.html" title="">
+                                                <img src="../../public/frontend/assets/upload/menu_01.jpg" alt="" class="img-fluid">
+                                                <div class="hovereffect">
+                                                    <span></span>
+                                                </div><!-- end hover -->
+                                            </a>
+                                        </div><!-- end media -->
+                                        <div class="blog-meta">
+                                            <span class="bg-grey"><a href="blog-category-01.html" title="">Spa</a></span>
+                                            <h4><a href="single.html" title="">What is your favorite leather color</a></h4>
+                                            <small><a href="blog-author.html" title="">By: Jessica</a></small>
+                                            <small><a href="blog-category-01.html" title="">21 July, 2017</a></small>
+                                        </div><!-- end meta -->
+                                    </div><!-- end blog-box -->
+                                </div><!-- end col -->
+
+                                <div class="pitem item-w1 item-h1">
+                                    <div class="blog-box">
+                                        <div class="post-media">
+                                            <a href="single.html" title="">
+                                                <img src="../../public/frontend/assets/upload/menu_02.jpg" alt="" class="img-fluid">
+                                                <div class="hovereffect">
+                                                    <span></span>
+                                                </div><!-- end hover -->
+                                            </a>
+                                        </div><!-- end media -->
+                                        <div class="blog-meta">
+                                            <span class="bg-grey"><a href="blog-category-01.html" title="">Beauty</a></span>
+                                            <h4><a href="single.html" title="">This year green glasses trend</a></h4>
+                                            <small><a href="blog-author.html" title="">By: Jenny</a></small>
+                                            <small><a href="blog-category-01.html" title="">19 July, 2017</a></small>
+                                        </div><!-- end meta -->
+                                    </div><!-- end blog-box -->
+                                </div><!-- end col -->
+
+                                <div class="pitem item-w1 item-h1">
+                                    <div class="blog-box">
+                                        <div class="post-media">
+                                            <a href="single.html" title="">
+                                                <img src="../../public/frontend/assets/upload/menu_03.jpg" alt="" class="img-fluid">
+                                                <div class="hovereffect">
+                                                    <span></span>
+                                                </div><!-- end hover -->
+                                            </a>
+                                        </div><!-- end media -->
+                                        <div class="blog-meta">
+                                            <span class="bg-grey"><a href="blog-category-01.html" title="">Spa</a></span>
+                                            <h4><a href="single.html" title="">Put on a hat for a better look</a></h4>
+                                            <small><a href="blog-author.html" title="">By: Amanda</a></small>
+                                            <small><a href="blog-category-01.html" title="">18 July, 2017</a></small>
+                                        </div><!-- end meta -->
+                                    </div><!-- end blog-box -->
+                                </div><!-- end col -->
+
+                                <div class="pitem item-w1 item-h1">
+                                    <div class="blog-box">
+                                        <div class="post-media">
+                                            <a href="single.html" title="">
+                                                <img src="../../public/frontend/assets/upload/menu_04.jpg" alt="" class="img-fluid">
+                                                <div class="hovereffect">
+                                                    <span></span>
+                                                </div><!-- end hover -->
+                                            </a>
+                                        </div><!-- end media -->
+                                        <div class="blog-meta">
+                                            <span class="bg-grey"><a href="blog-category-01.html" title="">Beauty</a></span>
+                                            <h4><a href="single.html" title="">We have been talking about one of the pioneers of male fashion</a></h4>
+                                            <small><a href="blog-author.html" title="">By: Jessica</a></small>
+                                            <small><a href="blog-category-01.html" title="">16 July, 2017</a></small>
+                                        </div><!-- end meta -->
+                                    </div><!-- end blog-box -->
+                                </div><!-- end col -->
+
+                                <div class="pitem item-w1 item-h1">
+                                    <div class="blog-box">
+                                        <div class="post-media">
+                                            <a href="single.html" title="">
+                                                <img src="../../public/frontend/assets/upload/menu_22.jpg" alt="" class="img-fluid">
+                                                <div class="hovereffect">
+                                                    <span></span>
+                                                </div><!-- end hover -->
+                                            </a>
+                                        </div><!-- end media -->
+                                        <div class="blog-meta">
+                                            <span class="bg-grey"><a href="blog-category-01.html" title="">Spa</a></span>
+                                            <h4><a href="single.html" title="">Things to watch out for when choosing sunglasses</a></h4>
+                                            <small><a href="blog-author.html" title="">By: Jenny</a></small>
+                                            <small><a href="blog-category-01.html" title="">14 July, 2017</a></small>
+                                        </div><!-- end meta -->
+                                    </div><!-- end blog-box -->
+                                </div><!-- end col -->
+
+                                <div class="pitem item-w1 item-h1">
+                                    <div class="blog-box">
+                                        <div class="post-media">
+                                            <a href="single.html" title="">
+                                                <img src="../../public/frontend/assets/upload/menu_23.jpg" alt="" class="img-fluid">
+                                                <div class="hovereffect">
+                                                    <span></span>
+                                                </div><!-- end hover -->
+                                            </a>
+                                        </div><!-- end media -->
+                                        <div class="blog-meta">
+                                            <span class="bg-grey"><a href="blog-category-01.html" title="">Spa</a></span>
+                                            <h4><a href="single.html" title="">A makeup made with honey and wax for your body</a></h4>
+                                            <small><a href="blog-author.html" title="">By: Amanda</a></small>
+                                            <small><a href="blog-category-01.html" title="">13 July, 2017</a></small>
+                                        </div><!-- end meta -->
+                                    </div><!-- end blog-box -->
+                                </div><!-- end col -->
+
+                                <div class="pitem item-w1 item-h1">
+                                    <div class="blog-box">
+                                        <div class="post-media">
+                                            <a href="single.html" title="">
+                                                <img src="../../public/frontend/assets/upload/menu_24.jpg" alt="" class="img-fluid">
+                                                <div class="hovereffect">
+                                                    <span></span>
+                                                </div><!-- end hover -->
+                                            </a>
+                                        </div><!-- end media -->
+                                        <div class="blog-meta">
+                                            <span class="bg-grey"><a href="blog-category-01.html" title="">Spa</a></span>
+                                            <h4><a href="single.html" title="">Fabulous treatment methods of nature</a></h4>
+                                            <small><a href="blog-author.html" title="">By: Martines</a></small>
+                                            <small><a href="blog-category-01.html" title="">12 July, 2017</a></small>
+                                        </div><!-- end meta -->
+                                    </div><!-- end blog-box -->
+                                </div><!-- end col -->
+
+                                <div class="pitem item-w1 item-h1">
+                                    <div class="blog-box">
+                                        <div class="post-media">
+                                            <a href="single.html" title="">
+                                                <img src="../../public/frontend/assets/upload/menu_25.jpg" alt="" class="img-fluid">
+                                                <div class="hovereffect">
+                                                    <span></span>
+                                                </div><!-- end hover -->
+                                            </a>
+                                        </div><!-- end media -->
+                                        <div class="blog-meta">
+                                            <span class="bg-grey"><a href="blog-category-01.html" title="">Beauty</a></span>
+                                            <h4><a href="single.html" title="">Shake your soap and candle-smelling body</a></h4>
+                                            <small><a href="blog-author.html" title="">By: Amanda</a></small>
+                                            <small><a href="blog-category-01.html" title="">11 July, 2017</a></small>
+                                        </div><!-- end meta -->
+                                    </div><!-- end blog-box -->
+                                </div><!-- end col -->
+
+                                <div class="pitem item-w1 item-h1">
+                                    <div class="blog-box">
+                                        <div class="post-media">
+                                            <a href="single.html" title="">
+                                                <img src="../../public/frontend/assets/upload/menu_26.jpg" alt="" class="img-fluid">
+                                                <div class="hovereffect">
+                                                    <span></span>
+                                                </div><!-- end hover -->
+                                            </a>
+                                        </div><!-- end media -->
+                                        <div class="blog-meta">
+                                            <span class="bg-grey"><a href="blog-category-01.html" title="">Spa</a></span>
+                                            <h4><a href="single.html" title="">Experience a unique experience in candlelight</a></h4>
+                                            <small><a href="blog-author.html" title="">By: Amanda</a></small>
+                                            <small><a href="blog-category-01.html" title="">11 July, 2017</a></small>
+                                        </div><!-- end meta -->
+                                    </div><!-- end blog-box -->
+                                </div><!-- end col -->
+                            </div><!-- end portfolio -->
+                        </div><!-- end page-wrapper -->
+
+                        <hr class="invis">
+
+                        <div class="row">
+                            <div class="col-md-12">
+                                <nav aria-label="Page navigation">
+                                    <ul class="pagination justify-content-start">
+                                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                        <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                        <li class="page-item">
+                                            <a class="page-link" href="#">Next</a>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </div><!-- end col -->
+                        </div><!-- end row -->
+                    </div><!-- end col -->
+                </div><!-- end row -->
+            </div><!-- end container -->
+        </section>
+
+    </div>
+</template>
+<script>
+export default {
+  name: "HealthPage",
+};
+</script>
