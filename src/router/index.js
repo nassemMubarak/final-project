eec8291003d415b63955201cd364ec9e168b4d8a
DevAhmedZiad <ahmed2588798@gmail.com>
2023-03-19 17:24:08 +0300

    remove files

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,6 @@ import HomePage from "../views/HomePage.vue";
 import PoliticsPage from "../views/PoliticsPage.vue";
 import EconomicPage from "../views/EconomicPage.vue";
 import SportPage from "../views/SportPage.vue";
-import TravelPage from "../views/TravelPage.vue";
-import VlogsPage from "../views/VlogsPage.vue";
-import HealthPage from "../views/HealthPage.vue";
 import DetailsNews from "../views/DetailsNews.vue";
 import AuthPage from "../views/AuthPage.vue";
 import OurTeam from "../views/OurTeam.vue";
@@ -28,24 +25,9 @@ const routes = [
     component: HomePage,
   },
   {
-    path: "/travel",
-    name: "TravelPage",
-    component: TravelPage,
-  },
-  {
-    path: "/health",
-    name: "HealthPage",
-    component: HealthPage,
-  },
-  {
     path: "/details",
     name: "DetailsNews",
     component: DetailsNews,
-  },
-  {
-    path: "/vlogs",
-    name: "VlogsPage",
-    component: VlogsPage,
   },
   {
     path: "/sport",
@@ -66,15 +48,6 @@ const routes = [
     path: "/ourTeam",
     name: "OurTeam",
     component: OurTeam,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
 ];
 
