e8f0b94f54b8214f02c58c06681d70e02320e042
DevAhmedZiad <ahmed2588798@gmail.com>
2023-03-20 10:37:31 +0300

    ahmed app

--- src/views/EditProfile.vue
+++ src/views/EditProfile.vue
@@ -14,96 +14,8 @@
                     />
                     <div class="mt-3">
                       <h4>Mohammed Kamal</h4>
-                      <p class="text-secondary mb-1">Full Stack Developer</p>
-                      <p class="text-muted font-size-sm">
-                        Palestine, Gaza, baytlahia
-                      </p>
-                      <button class="btn btn-primary" style="margin-right: 5px;">Follow</button>
-                      <button class="btn btn-outline-primary" style="margin-left: 5px;">Message</button>
                     </div>
-                  </div>
-                  <hr class="my-4" />
-                  <ul class="list-group list-group-flush">
-                    
-                    <li
-                      class="list-group-item d-flex justify-content-between align-items-center flex-wrap"
-                    >
-                      <h6 class="mb-0">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="24"
-                          height="24"
-                          viewBox="0 0 24 24"
-                          fill="none"
-                          stroke="currentColor"
-                          stroke-width="2"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          class="feather feather-twitter me-2 icon-inline text-info"
-                        >
-                          <path
-                            d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"
-                          ></path></svg
-                        >Twitter
-                      </h6>
-                      <span class="text-secondary">@bootdey</span>
-                    </li>
-                    <li
-                      class="list-group-item d-flex justify-content-between align-items-center flex-wrap"
-                    >
-                      <h6 class="mb-0">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="24"
-                          height="24"
-                          viewBox="0 0 24 24"
-                          fill="none"
-                          stroke="currentColor"
-                          stroke-width="2"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          class="feather feather-instagram me-2 icon-inline text-danger"
-                        >
-                          <rect
-                            x="2"
-                            y="2"
-                            width="20"
-                            height="20"
-                            rx="5"
-                            ry="5"
-                          ></rect>
-                          <path
-                            d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"
-                          ></path>
-                          <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg
-                        >Instagram
-                      </h6>
-                      <span class="text-secondary">bootdey</span>
-                    </li>
-                    <li
-                      class="list-group-item d-flex justify-content-between align-items-center flex-wrap"
-                    >
-                      <h6 class="mb-0">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="24"
-                          height="24"
-                          viewBox="0 0 24 24"
-                          fill="none"
-                          stroke="currentColor"
-                          stroke-width="2"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          class="feather feather-facebook me-2 icon-inline text-primary"
-                        >
-                          <path
-                            d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"
-                          ></path></svg
-                        >Facebook
-                      </h6>
-                      <span class="text-secondary">bootdey</span>
-                    </li>
-                  </ul>
+                  </div>                  
                 </div>
               </div>
             </div>
@@ -130,18 +42,7 @@
                       />
                     </div>
                   </div>
-                  <div class="row mb-3">
-                    <div class="col-sm-3">
-                      <h6 class="mb-0">Phone</h6>
-                    </div>
-                    <div class="col-sm-9 text-secondary">
-                      <input
-                        type="text"
-                        class="form-control"
-                        value="(239) 816-9029"
-                      />
-                    </div>
-                  </div>
+                  
                   <div class="row mb-3">
                     <div class="col-sm-3">
                       <h6 class="mb-0">Mobile</h6>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -14,17 +14,13 @@
                     />
                     <div class="mt-3">
                       <h4>Mohammed Kamal</h4>
-                      <p class="text-secondary mb-1">Full Stack Developer</p>
-                      <p class="text-muted font-size-sm">
-                        Palestine, Gaza, baytlahia
-                      </p>
-                      <button class="btn btn-primary" style="margin-right: 5px;">Follow</button>
-                      <button class="btn btn-outline-primary" style="margin-left: 5px;">Message</button>
+                      <!-- <button class="btn btn-primary" style="margin-right: 5px;">Follow</button>
+                      <button class="btn btn-outline-primary" style="margin-left: 5px;">Message</button> -->
                     </div>
                   </div>
                 </div>
               </div>
-              <div class="card mt-3">
+              <!-- <div class="card mt-3">
                 <ul class="list-group list-group-flush">
                   <li
                     class="list-group-item d-flex justify-content-between align-items-center flex-wrap"
@@ -105,7 +101,7 @@
                     <span class="text-secondary">bootdey</span>
                   </li>
                 </ul>
-              </div>
+              </div> -->
             </div>
             <div class="col-md-8">
               <div class="card mb-3">
@@ -124,13 +120,7 @@
                     <div class="col-sm-9 text-secondary">fip@jukmuh.al</div>
                   </div>
                   <hr />
-                  <div class="row">
-                    <div class="col-sm-3">
-                      <h6 class="mb-0">Phone</h6>
-                    </div>
-                    <div class="col-sm-9 text-secondary">(239) 816-9029</div>
-                  </div>
-                  <hr />
+                  
                   <div class="row">
                     <div class="col-sm-3">
                       <h6 class="mb-0">Mobile</h6>
